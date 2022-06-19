bdc342fb03f1283f25d9deb6ac504772ee978874
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-06-19 02:58:05 -0400

    compiled question responses into single object

--- index.js
+++ index.js
@@ -4,59 +4,50 @@ const inquirer = require("inquirer");
 // const { writeFile, copyFile } = require("./utils/manage_files");
 const { initial_questions, contribution_questions, tests_questions, mock_data} = require("./Develop/utils/questions.js");
 
-//objects we need for processing
-let initial_answers = [];
-let contributions = [];
-let tests = [];
-
-// TODO: Create an array of questions for user input
-// const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
 const promptInitQuestions = () => {
   return inquirer.prompt(initial_questions);
 };
 
-const promptContributions = () => {
+const promptContributions = readme_data => {
+  if(!readme_data.contributions){
+    readme_data.contributions = [];
+  }
   return inquirer.prompt(contribution_questions)
-    .then((contribution_answer) => {
-    contributions.push(contribution_answer.contribution);
+    .then(contribution_answer => {
+    readme_data.contributions.push(contribution_answer.contribution);
     if(contribution_answer.moreContributions) {
-      return promptContributions();
+      return promptContributions(readme_data);
     } else {
-      return contributions;
+      return readme_data;
     }
   });
 };
 
-const promptTests = () => {
+const promptTests = readme_data => {
+  if(!readme_data.tests)
+    readme_data.tests = [];
   return inquirer.prompt(tests_questions)
     .then((test_answer) => {
-    tests.push(test_answer.test);
+    readme_data.tests.push(test_answer.test);
     if (test_answer.moreTests) {
-      return promptTests();
+      return promptTests(readme_data);
     } else {
-      return contributions;
+      return readme_data;
     }
   });
 };
 
-// Function call to initialize app
-promptInitQuestions()
-  .then((answers) => {
-    initial_answers = answers;
-  })
-  .then(promptContributions)
-  .then(promptTests)
-  .then(() => {
-    console.log(initial_answers);
-    console.log(contributions);
-    console.log(tests);
-  });
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-//console.log(JSON.stringify(answers.contributionCount, null, "  "));
+// Function call to initialize app
+function init() {
+  promptInitQuestions()
+    .then(promptContributions)
+    .then(promptTests)
+    .then (readme_data => {
+      console.log(readme_data);
+    });
+}
+
+init();
